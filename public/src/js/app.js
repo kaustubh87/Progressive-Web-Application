e387ad955ef1a2392ec84364307d05c1fa124ee6
kvin1087 <kaustubh87@gmail.com>
2017-09-28 22:27:12 -0500

    Updated service worker code to work on supported browsers

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,7 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('/sw.js');
+        .register('/sw.js')
+        .then(function() {
+            console.log('Service Worker registered');
+        });
 }
